c59d3989e5069d9437515e9774a3ce903ce0d4af
levon-d <mycroft2003@gmail.com>
2024-11-03 23:46:11 +0000

    rotate string

---
+++ strings/rotate_string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        
+        doubled_s = s + s
+        return goal in doubled_s
